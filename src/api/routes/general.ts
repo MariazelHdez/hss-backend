216394c45c2a26da116eb5686e137fcc5a0af434
EC2 Default User <ec2-user@ip-172-31-15-146.us-west-2.compute.internal>
2025-03-27 16:17:05 +0000

    update labels

--- src/api/routes/general.ts
+++ src/api/routes/general.ts
@@ -215,7 +215,7 @@ generalRouter.post("/logs", async (req: Request, res: Response) => {
 
         let query = db(`${SCHEMA_GENERAL}.SUBMISSIONS_LOGS`)
             .whereIn('SCHEMA_NAME', moduleName)
-            .orderBy('ID', 'ASC');
+            .orderBy('ID', 'DESC');
 
         if (userId) {
             const userRow = await db(`${SCHEMA_GENERAL}.USER_DATA`)
@@ -248,7 +248,7 @@ generalRouter.post("/logs", async (req: Request, res: Response) => {
                 [dateFrom, dateTo]));
         }
 
-        if (firstName) {
+        if (firstName && firstName.trim() !== '') {
             const lowerSearchFn = firstName.trim().toLowerCase();
 
             query.where(function () {
@@ -256,7 +256,7 @@ generalRouter.post("/logs", async (req: Request, res: Response) => {
             });
         }
 
-        if (lastName) {
+        if (lastName && lastName.trim() !== '') {
             const lowerSearchLn = lastName.trim().toLowerCase();
 
             query.where(function () {
